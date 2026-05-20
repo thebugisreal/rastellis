d37011da66a93128cbffc57b54dec75714f55b6d
Phuong Nguyen <phuong.nguyen@barreldev.com>
2026-05-20 18:02:58 +0700

    RAS-179 Update styles featured colletion slider on homepage

--- frontend/scripts/sections/featured-collection-slider.js
+++ frontend/scripts/sections/featured-collection-slider.js
@@ -1,6 +1,5 @@
 import section from '@/scripts/glow/section'
 import { qs, qsa, listen, add, remove, contains } from '@fluorescent/dom'
-import { emit, on } from 'evx'
 
 import Carousel from '@/scripts/lib/carousel'
 import ProductItem from '@/scripts/lib/product-item'
@@ -9,8 +8,8 @@ import shouldAnimate from '@/scripts/lib/animation/shouldAnimate'
 
 const selectors = {
   navItems: '.featured-collection-slider__navigation-list-item',
-  sliderContainer: '.carousel',
-  navButtons: '.carousel__navigation-buttons',
+  sliderContainer: '.featured-collection-slider__content.carousel',
+  navControls: '.featured-collection-slider__carousel-controls',
 }
 
 const classes = {
@@ -33,19 +32,19 @@ section('featured-collection-slider', {
   },
 
   _initCarousels() {
-    const { productsPerView, mobileProductsPerView } = this.container.dataset
+    const { productsPerView, mobileProductsPerView, showCarouselControls } =
+      this.container.dataset
+    const enableCarouselControls = showCarouselControls !== 'false'
     this.perView = parseInt(productsPerView, 10)
-    this.mobilePerView = parseInt(mobileProductsPerView, 10) * 1.05
-    // 1.05 factor gives us a "peek" without CSS hacks
-    // TODO: encapsulate this in carousel instead of duplication wherever
-    // we call on carousel.  Can also simplify the config that we pass in
-    // to something like perViewSmall, perViewMedium, perViewLarge and same with
-    // spaceBetween?
+    const mobileColumns = parseFloat(mobileProductsPerView, 10)
+    this.mobilePerView = Number.isInteger(mobileColumns)
+      ? mobileColumns * 1.05
+      : mobileColumns
 
     this.productItem = ProductItem(this.container)
     this.carouselsElements = qsa(selectors.sliderContainer, this.container)
     this.navItems = qsa(selectors.navItems, this.container)
-    this.navigationButtons = qsa(selectors.navButtons, this.container)
+    this.navControls = qsa(selectors.navControls, this.container)
 
     this.navItems.forEach((button) =>
       this.events.push(
@@ -58,30 +57,47 @@ section('featured-collection-slider', {
         `[data-navigation="${index}"]`,
         this.container,
       )
-      const nextButton = qs('[data-next]', navigationWrapper)
-      const prevButton = qs('[data-prev]', navigationWrapper)
+      const nextButton = enableCarouselControls
+        ? qs('[data-next]', navigationWrapper)
+        : null
+      const prevButton = enableCarouselControls
+        ? qs('[data-prev]', navigationWrapper)
+        : null
+      const paginationEl = enableCarouselControls
+        ? qs('.swiper-pagination', navigationWrapper)
+        : null
 
-      this.carousels.push(
-        Carousel(container, {
-          slidesPerView: this.mobilePerView,
-          spaceBetween: 13, // matches product grid
-          navigation: {
-            nextEl: nextButton,
-            prevEl: prevButton,
+      const carouselOptions = {
+        slidesPerView: this.mobilePerView,
+        spaceBetween: 16, // matches product grid
+        breakpoints: {
+          720: {
+            spaceBetween: 24, // matches product grid
+            slidesPerView:
+              this.perView === 5 ? this.perView - 1 : this.perView,
           },
-          breakpoints: {
-            720: {
-              spaceBetween: 17, // matches product grid
-              slidesPerView:
-                this.perView === 5 ? this.perView - 1 : this.perView,
-            },
-            1200: {
-              spaceBetween: 25, // matches product grid
-              slidesPerView: this.perView,
-            },
+          1200: {
+            spaceBetween: 32, // matches product grid
+            slidesPerView: this.perView,
           },
-        }),
-      )
+        },
+      }
+
+      if (enableCarouselControls && nextButton && prevButton) {
+        carouselOptions.navigation = {
+          nextEl: nextButton,
+          prevEl: prevButton,
+        }
+      }
+
+      if (enableCarouselControls && paginationEl) {
+        carouselOptions.pagination = {
+          el: paginationEl,
+          clickable: true,
+        }
+      }
+
+      this.carousels.push(Carousel(container, carouselOptions))
     })
   },
 
@@ -97,7 +113,7 @@ section('featured-collection-slider', {
 
   _hideAll() {
     remove(this.navItems, classes.selected)
-    remove(this.navigationButtons, classes.visible)
+    remove(this.navControls, classes.visible)
 
     remove(this.carouselsElements, classes.initReveal)
     remove(this.carouselsElements, classes.reveal)
@@ -113,9 +129,15 @@ section('featured-collection-slider', {
   },
 
   _show(index) {
-    const navigationWrapper = qs(`[data-navigation="${index}"]`, this.container)
-    add(navigationWrapper, classes.visible)
     const collection = qs(`[data-collection="${index}"]`, this.container)
+
+    if (this.navControls.length) {
+      const navigationWrapper = qs(
+        `[data-navigation="${index}"]`,
+        this.container,
+      )
+      add(navigationWrapper, classes.visible)
+    }
 
     if (this.navItems.length) {
       const navigationItem = qs(
