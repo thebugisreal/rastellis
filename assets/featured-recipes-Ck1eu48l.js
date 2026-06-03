9ad1dd51d9674316b070760e9ee798b01a059bad
Felipe Fanucchi <felipe.fanucchi@barrelny.com>
2026-06-03 11:37:26 -0300

    Build Assets

---
+++ assets/featured-recipes-Ck1eu48l.js
@@ -0,0 +1,1 @@
+import{_ as r,r as s}from"./index-BWeMscSV.js";import{C as o}from"./carousel-eu4IY4Zw.js";r(()=>import("./video-control-Dh429iUr.js"),[],import.meta.url);s("featured-recipes",{onLoad(){this.carousel=o(this.container,{slidesPerView:1.1,spaceBetween:16,threshold:5,breakpoints:{960:{slidesPerView:4,spaceBetween:32}}})},onUnload(){var e;(e=this.carousel)==null||e.destroy()}});
